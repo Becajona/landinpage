2814bba715b9ec3382d77c4fee1dc1bcea656cdd
Jonathan V <jonathanvegagomez15@gmail.com>
2026-01-23 20:54:02 -0600

    Funcioness

--- app/admin/login/page.tsx
+++ app/admin/login/page.tsx
@@ -1,6 +1,8 @@
 // app/admin/login/page.tsx
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { signInWithEmailAndPassword } from "firebase/auth";
